238eebd1b4eef1ce7bac0e3d45cfa3753c32e93e
JosueHernandezR <justinhernandez054@gmail.com>
2019-09-27 15:03:47 -0500

    Registros

--- Práctica 1/ALU.vhdl
+++ Práctica 1/ALU.vhdl
@@ -2,10 +2,10 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
  
-entity alu is
+entity ALU is
  Port ( inp_a : in signed(3 downto 0);
  inp_b : in signed(3 downto 0);
- sel : in STD_LOGIC_VECTOR (2 downto 0);
+ sel : in STD_LOGIC_VECTOR (3 downto 0);
  out_alu : out signed(3 downto 0));
 end alu;
  
@@ -14,21 +14,21 @@ begin
     process(inp_a, inp_b, sel)
     begin
         case sel is
-            when "000" =>
+            when "0000" =>
             out_alu<= inp_a + inp_b; --addition
-            when "001" =>
+            when "0001" =>
             out_alu<= inp_a - inp_b; --subtraction
-            when "010" =>
+            when "0010" =>
             out_alu<= inp_a - 1; --sub 1
-            when "011" =>
+            when "0011" =>
             out_alu<= inp_a + 1; --add 1
-            when "100" =>
+            when "0100" =>
             out_alu<= inp_a and inp_b; --AND gate
-            when "101" =>
+            when "0101" =>
             out_alu<= inp_a or inp_b; --OR gate
-            when "110" =>
+            when "0110" =>
             out_alu<= not inp_a ; --NOT gate
-            when "111" =>
+            when "0111" =>
             out_alu<= inp_a xor inp_b; --XOR gate
             when others =>
             NULL;
